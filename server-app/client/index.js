1ed1aa4def31fce879a0eca52bf292228aba0566
hamzajaved2080 <hamza.bsse2080@iiu.edu.pk>
2017-05-11 22:59:04 +0500

    html template basic

--- server-app/client/index.js
+++ server-app/client/index.js
@@ -1,25 +1,34 @@
-// var ejs = require('ejs');
-    // var app = require('../server/server.js');
-//     var model_schema = new Array();
-// var json_files = new Array();
-// const testFolder = '../common/models/';
-// const fs = require('fs');
-// fs.readdir(testFolder, (err, files) => {
-//   files.forEach(function(file) {
-//       if ( file.split('.').pop() == 'json')
-//       json_files.push(file);
-//   });
-//   for (var i=0 ;i<json_files.length; i++){
-//     model_schema.push(require('../common/models/' + json_files[i]));
-// }
-//  console.log(model_schema)
-// });
 var ejs = require('ejs');
-var fs = require('fs');
- var abc = require('../common/models/items.json');
-var temp = fs.readFileSync('./index.ejs' , 'utf-8');
-console.log(abc)
-     html = ejs.render(temp,{abc : abc});
-console.log(html);
+var app = require('../server/server.js');
+var model_schema = new Array();
+var json_files = new Array();
+const testFolder = '../common/models/';
+const fs = require('fs');
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(function (file) {
+    if (file.split('.').pop() == 'json')
+      json_files.push(file);
+  });
+  for (var i = 0; i < json_files.length; i++) {
+    model_schema.push(require('../common/models/' + json_files[i]));
+  }
+//   console.log(model_schema)
+var model,temp;
+for (var count=0 ; count<model_schema.length ; count++){
+    temp = fs.readFileSync('./modelTemplate.ejs' , 'utf-8');
+    model = model_schema[count];
+    html = ejs.render(temp,{model : model});
+    console.log(html);
+    fs.writeFileSync(model.name+'.html', html, 'utf-8')
 
-fs.writeFileSync('index2.html',html , 'utf-8')
+}
+});
+
+//  var abc = require('../common/models/items.json');
+// var temp = fs.readFileSync('./modelTemplate.ejs' , 'utf-8');
+// console.log(abc)
+    //  html = ejs.render(temp,{temp : abc});
+// console.log(html);
+// console.log(temp , model[0].name+'.html');
+
+// fs.writeFileSync(model[0].name+'.html', html, 'utf-8')
